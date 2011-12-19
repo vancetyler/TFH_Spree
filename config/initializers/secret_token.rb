9c64345901071d4c55f877f0b7c38394874588bc
vancetyler <vance.tyler@gmail.com>
2011-12-19 15:05:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree4::Application.config.secret_token = '0741c28748e1da3b61546645a531058f37559a684c11b6a9eb8a1e2de19c71f90b38b72f08379f3c65efb1e7cad2b1e718e108e18ea6cc525a04be7bd78244a3'
---
+++ config/initializers/spree_config.rb
@@ -0,0 +1,1 @@
+Spree::Config.set(:logo => "store/logo.png")
