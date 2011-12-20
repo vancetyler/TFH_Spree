28d10d0f3d4b6e8c956bd08c8b06554b6c2e1816
vancetyler <vance.tyler@gmail.com>
2011-12-20 13:28:30 -0800

    Capified.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,13 @@
 set :application, "TFH Webstore"
-set :repository,  "set your repository location here"
-
+set :repository,  "git@github.com:vancetyler/TFH_Spree.git"
+set :scm, "git"
+set :user, "spree"
+set :scm_passphrase, "tRMCMvl6uvop"
+set :branch, "master"
+set :deploy_via, :checkout
+set :git_shallow_clone, 1
+set :use_sudo, false
+set :deploy_to, "/home//apps/store"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
@@ -10,6 +17,6 @@ set :repository,  "set your repository location here"
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "http://tfh.spreeworks.com/"
+role :web, "http://tfh.spreeworks.com/"
+role :db,  "http://tfh.spreeworks.com/", :primary => true
